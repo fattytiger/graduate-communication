430bb7a6f9a43674d21302530123ba324c7400c6
panghu <315782792@qq.com>
2020-03-05 22:36:41 +0800

    Init the openid as global data at begining

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,7 +1,9 @@
 //app.js
 App({
   onLaunch: function () {
-    
+    this.globalData = {}
+
+
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
@@ -10,6 +12,15 @@ App({
       })
     }
 
-    this.globalData = {}
+    wx.cloud.callFunction({
+      name:'init_page',
+      success:(result)=>{
+        this.globalData.openid = result.result.openid
+      }
+    }) 
+
+
+    
+
   }
 })
