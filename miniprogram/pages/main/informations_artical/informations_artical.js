3a28e1959c86a8c0933d437d998cac282ddcfbd0
panghu <315782792@qq.com>
2020-03-07 15:07:04 +0800

    Render the information artical in information page

--- miniprogram/pages/main/informations_artical/informations_artical.js
+++ miniprogram/pages/main/informations_artical/informations_artical.js
@@ -1,11 +1,18 @@
 // pages/main/informations_artical/informations_artical.js
+const db = wx.cloud.database()
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    title:'',
+    author:'',
+    desc:'',
+    comments:[],
+    textValue:'',
+    flagNum:0
   },
 
   /**
@@ -13,54 +20,86 @@ Page({
    */
   onLoad: function (options) {
 
+    //get the artical id
+    this.articalID = options.artical_id
+
+    db.collection('information_collection').where({
+      _id:this.articalID
+    }).get().then(result => {
+      //get finddata
+      let finddata = result.data[0]
+      //render the page by received data
+      this.data.title = finddata.title
+      this.data.author = finddata.author
+      this.data.desc = finddata.desc
+      this.data.comments = finddata.comments
+      this.setData({
+        title:this.data.title,
+        author:this.data.author,
+        desc:this.data.desc,
+        comments:this.data.comments
+      })
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onPullDownRefresh:function(){
+    db.collection('information_collection').where({
+      _id:this.articalID
+    }).get().then(res => {
+      let finddata = res.data[0]
+      this.data.comments = finddata.comments
+      this.setData({
+        comments:this.data.comments
+      })
+      //cancle loading
+      wx.stopPullDownRefresh({
+        success: (res) => {
+          wx.hideLoading()
+        },
+      })
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  inputComment:function(e){
+    this.data.textValue = e.detail.value
+    this.setData({
+      flagNum:this.data.textValue.length
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  addComment:function(){
+    //judge the comment is empty or not
+    if(this.data.textValue === ''){
+      wx.showToast({
+        title: '请输入评论',
+      })
+      return
+    }
+    wx.showLoading({
+      title: '请稍后',
+    })
+    const _ = db.command
+    db.collection('information_collection').doc(`${this.articalID}`).update({
+      data:{
+        comments:_.push({
+          author:app.globalData.nickname,
+          comment:this.data.textValue
+        })
+      }
+    }).then(res => {
+      //hide loading
+      wx.hideLoading({
+        success: (res) => {
+          wx.startPullDownRefresh({
+            success: (res) => {
+              wx.showLoading({
+                title: '正在更新',
+              })
+            },
+          })
+        },
+      })
+      console.log(res)
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
