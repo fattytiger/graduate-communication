83ad9f0e7172f2874cf3ca37419453a21c7e6a77
panghu <315782792@qq.com>
2020-03-07 15:04:49 +0800

    Render the experience on the page and add experience function in experience.js

--- miniprogram/pages/main/experience/experience.js
+++ miniprogram/pages/main/experience/experience.js
@@ -1,66 +1,114 @@
 // pages/main/experience/experience.js
+const app = getApp()
+const db = wx.cloud.database()
+const transferDate = require('../../../utils/formateDate')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    textareaValue:'',
+    title:'',
+    experiences:[],
+    flagNum:0
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  
   onLoad: function (options) {
-
+    db.collection('experience_collection').get().then(result => {
+      console.log(result)
+      this.data.experiences = result.data
+      this.setData({
+        experiences:this.data.experiences
+      })
+    })
+  },
+  onPullDownRefresh:function(){
+    db.collection('experience_collection').get().then(result => {
+      this.data.experiences = result.data
+      this.setData({
+        experiences:this.data.experiences
+      })
+      wx.stopPullDownRefresh({
+        success:()=>{
+          this.data.title = ""
+          this.data.textareaValue = ''
+          this.setData({
+            title:this.data.title,
+            textareaValue:this.data.textareaValue
+          })
+          wx.hideLoading()
+        }
+      })
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
+  inputTextContent:function(e){
+    this.data.textareaValue = e.detail.value
+    //show the text words number
+    this.setData({
+      flagNum: this.data.textareaValue.length
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  inputExpeTitle:function(e){
+    this.data.title = e.detail.value
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+  addExperience:function(){
+    //judge the title is empty or not 
+    if(this.data.title === ''){
+      wx.showToast({
+        title: '请填写标题',
+        icon:'none'
+      })
+      return
+    }
 
-  },
+    //judege the data is empty or not
+    if(this.data.textareaValue === ''){
+      wx.showToast({
+        title: '请填写经验',
+        icon:'none'
+      })
+      return
+    }
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+    if(this.data.textareaValue.length < 10){
+      wx.showToast({
+        title: '输入字数不小于10',
+        icon:'none'
+      })
+      return
+    }
+    wx.showLoading({
+      title: '请稍等',
+    })
+    
+    const date = new Date()
+    const transferdata = transferDate.transferDate(date)
 
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    db.collection('experience_collection').add({
+      data:{
+        title:this.data.title,
+        desc:this.data.textareaValue,
+        time:transferdata,
+        author:app.globalData.nickname
+      }
+    }).then(result => {
+      wx.hideLoading({
+        success:function(){
+          wx.startPullDownRefresh({
+            success:function(){
+              wx.showLoading({
+                title: '开始更新',
+              })
+            }
+          })
+        }
+      })
+    })
   }
 })
