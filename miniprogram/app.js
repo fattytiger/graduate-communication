a43f34d80d1eed092542afb0ac8566b32cb71eea
panghu <315782792@qq.com>
2020-03-07 15:10:07 +0800

    Init global data in app.js

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -2,8 +2,6 @@
 App({
   onLaunch: function () {
     this.globalData = {}
-
-
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
@@ -11,16 +9,20 @@ App({
         traceUser: true,
       })
     }
+    //init global data
+    const db = wx.cloud.database()
 
     wx.cloud.callFunction({
       name:'init_page',
       success:(result)=>{
         this.globalData.openid = result.result.openid
+
+        db.collection('user_collection').where({
+          openid:this.globalData.openid
+        }).get().then(document => {
+          this.globalData.nickname = document.data[0].nickname
+        })
       }
     }) 
-
-
-    
-
   }
 })
